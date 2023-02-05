441e0d6111f7d99c284f2751d1979f2cc8ea96d7
Shulkevich Vladislav <shulkevich@gmail.com>
2023-02-05 13:42:56 +0300

    структура dds слоя

---
+++ sql/02_dds.sql
@@ -0,0 +1,13 @@
+
+
+create table dds.events {
+    id serial primary key,
+    --	2020-07-05 14:32:45.407110
+	event_timestamp timestamp,
+    -- час события
+    hour integer,
+    -- суррогатный тип события  (pageview, purchase)
+    event_type varchar,
+    -- ссылка на струницу, откуда пользователь пришел
+    page_url varchar
+};
