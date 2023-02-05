97b7385af960a250569f3c22600ac9ab428a0580
Shulkevich Vladislav <shulkevich@gmail.com>
2023-02-05 13:39:34 +0300

    структура витрин

--- sql/03_datamart.sql
+++ sql/03_datamart.sql
@@ -1,10 +1,25 @@
--- 1. распределение событий по часам; 
+-- 1. распределение событий по часам;
 
-create table datamart.01_events {
-};
+create table datamart.01_events (
+    id serial primary key,
+    hour integer,
+    event_type varchar,
+    event_count integer
+);
 
 -- 2. количество купленных товаров в разрезе часа;
-create table datamart.02_
+create table datamart.02_goods (
+    id serial primary key,
+    hour integer,
+    event_count integer
+);
 
 
 -- 3. топ-10 посещённых страниц, с которых был переход в покупку — список ссылок с количеством покупок.
+
+-- id формируется из row_number
+
+create table datamart.03_links (
+    id integer primary key,
+    page_url varchar
+);
